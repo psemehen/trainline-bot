b02cdfcff1628876cb225d50cd214e3865aa5ad7
Paul Semehen <psemehen@gmail.com>
2025-03-07 04:08:56 +0100

    Indicate immutable

--- errors/journey_error.rb
+++ errors/journey_error.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Errors
   class JourneyError < StandardError
     attr_reader :errors
--- errors/location_error.rb
+++ errors/location_error.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Errors
   class LocationError < StandardError
     attr_reader :errors
